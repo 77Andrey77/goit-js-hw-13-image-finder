0be0a5039dced7270cfdc93c5b52479e1678b853
Andrii Pysmennyi <andrey.ad511989@gmail.com>
2020-11-15 16:06:07 +0200

    add

--- src/apiService.js
+++ src/apiService.js
@@ -1,7 +1,45 @@
-const BASE_URL = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal';
-const KEY = '19032313-4dd4b1c57c2e902bf9f549139';
+export default class newApiService{
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+    }
+    feachArticales() {
+        // console.log(this);
+    const BASE_URL = 'https://pixabay.com/api';
+    const KEY = '19032313-4dd4b1c57c2e902bf9f549139';
+ 
+return fetch(`${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${KEY}`)
+        .then(response => response.json())
+    .then(data => {
+        // console.log(data);
+        this.incrementPage();
 
-export default function fetchApiService(searchQuery) {
-    return fetch(`${BASE_URL}&q=что_искать&page=номер_страницы&per_page=12&key=${KEY}`)
-        .then(response => response.json());
+        return data.hits;
+    })
+    }
+
+    incrementPage() {
+     this.page += 1;
 }
+    resetPage() {
+        this.page = 1;
+}
+    
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+}
+
+//  const BASE_URL = 'https://pixabay.com/api';
+// const KEY = '19032313-4dd4b1c57c2e902bf9f549139';
+
+// export default function fetchApiService(searchQuery) {
+// return
+// fetch(`${BASE_URL}/?image_type=photo&orientation=horizontal&q=cat&page=1&per_page=12&key=${KEY}`)
+//         .then(response => response.json())
+//     .then(console.log)
+// };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,47 @@
 import "./css/common.css";
+import cardTpl from "./templates/photo-card.hbs";
+import NewsApiService from "./apiService"
 
-const refs = {
-    form: document.querySelector('.search-form'),
-    gallery: document.querySelector('.gallery'),
+// var debounce = require('lodash.debounce');
+// import debounce from "lodash.debounce";
+
+
+const formEl = document.querySelector('.search-form');
+const inputEl = document.querySelector('.form-control');
+const galleryEl = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('[data-action="load-more"]');
+
+const newsApiService = new NewsApiService();
+
+formEl.addEventListener('submit', onSearch);
+loadMoreBtn.addEventListener('click', onLoadMore);
+
+
+function onSearch(e) {
+    e.preventDefault();
+
+    creatHitsContainer();
+    newsApiService.query = e.currentTarget.elements.query.value;
+    
+    if (newsApiService.query === "") {
+        return alert('Введите нормальный запрос!!!')
+    }
+    newsApiService.resetPage();
+    newsApiService.feachArticales().then(appendHitsMarkup);
+};
+
+function onLoadMore() {
+    newsApiService.feachArticales().then(appendHitsMarkup);
+      
 }
+ 
+function appendHitsMarkup(hits) {
+    galleryEl.insertAdjacentHTML('beforeend', cardTpl(hits));    
+}
+
+function creatHitsContainer() {
+    galleryEl.innerHTML = '';    
+}
+
 
 
